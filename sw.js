807773d83a78611ad73ff2521587fcfc732d804d
ayudora <ayudora1105@gmail.com>
2026-04-21 21:08:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE_NAME = 'sou-log-v1';
+const CACHE_NAME = 'sou-log-v2'; // バージョンを上げました
 const urlsToCache = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
+// インストール時にキャッシュ
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -12,6 +16,22 @@ self.addEventListener('install', (event) => {
   );
 });
 
+// 新しいバージョンを即座に有効化
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// フェッチ（データ取得）時にキャッシュがあればそれを返す
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
